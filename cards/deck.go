c6a64fa6b2f0f29dcd8a7f282eb672435330ae7f
guilam34 <guillaumelam34@gmail.com>
2018-12-08 15:01:02 -0800

    Basic Deck class

---
+++ cards/deck.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+// All deck objects get access to this method
+func (cards deck) print() {
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
