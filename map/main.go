71e1d076cce42c123e41d158e44990b0197f0310
guilam34 <guillaumelam34@gmail.com>
2018-12-08 17:38:00 -0800

    Remove item from map

--- map/main.go
+++ map/main.go
@@ -5,10 +5,9 @@ import (
 )
 
 func main() {
-	colors := map[string]string{
-		"red":   "ff0000",
-		"green": "#4bf745",
-	}
+	colors := make(map[int]string)
+	colors[10] = "#ffffff"
+	delete(colors, 10)
 
 	fmt.Printf("%+v", colors)
 }
