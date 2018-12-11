b25855de94300e0033d7d80fa6b63372fc0f6eec
guilam34 <guillaumelam34@gmail.com>
2018-12-10 16:59:55 -0800

    Blocking channels wait for all

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,9 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	// blocking call, waits for first message sent to channel
-	fmt.Println(<-c)
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
 func checkLink(link string, c chan string) {
